98e2a8a2b3e17a1208bcf4121f98ab397f97a62a
ahsanhabibakik <syedmirhabib@gmail.com>
2025-08-25 09:06:18 +0600

    feat: add enhanced Hotjar component with better UX

---
+++ src/components/analytics/Hotjar.tsx
@@ -0,0 +1,28 @@
+import Script from 'next/script'
+
+export const Hotjar = () => {
+  const hjid = process.env.NEXT_PUBLIC_HOTJAR_ID
+
+  if (!hjid) {
+    return null
+  }
+
+  return (
+    <Script
+      id="hotjar-script"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:${hjid},hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `,
+      }}
+    />
+  )
+}
