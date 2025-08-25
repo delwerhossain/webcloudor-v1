a7541ab5dec78d6116afd80fdc8c452cfd2e5e03
ahsanhabibakik <syedmirhabib@gmail.com>
2025-08-25 09:09:18 +0600

    feat: add analytics components for tracking and user behavior analysis

---
+++ src/components/analytics/index.ts
@@ -0,0 +1,5 @@
+// Analytics components for tracking and user behavior analysis
+export { GoogleAnalytics } from './GoogleAnalytics'
+export { GoogleTagManager } from './GoogleTagManager'
+export { Hotjar } from './Hotjar'
+export { MicrosoftClarity } from './MicrosoftClarity'
